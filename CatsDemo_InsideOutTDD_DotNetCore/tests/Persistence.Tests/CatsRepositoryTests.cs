2987fce4e1253488a792c4f4414422d16988817e
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2017-09-02 00:05:02 +0800

    cats repository

---
+++ CatsDemo_InsideOutTDD_DotNetCore/tests/Persistence.Tests/CatsRepositoryTests.cs
@@ -0,0 +1,27 @@
+using Domain;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Persistence.Tests
+{
+    public class CatsRepositoryTests
+    {
+        [Fact]
+        public void ShouldSaveNewCatIntoTheDatabase()
+        {
+            // arrange
+            var repository = new CatsRepository("cats.db");
+            var cat = new Cat { Name = "Kang Kang" };
+
+            // act
+            repository.Save(cat);
+
+            // assert
+            using (StreamReader reader = File.OpenText("cats.db"))
+            {
+                Assert.Contains("Kang Kang", reader.ReadLine());
+            }
+        }
+    }
+}
