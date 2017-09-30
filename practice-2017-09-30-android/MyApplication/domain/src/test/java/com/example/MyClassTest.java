a5c6cfb2d0849a65141be8df27667e5cb30d1394
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2017-09-30 22:27:36 +0800

    added new module named "domain"; first test to test that tests are working

---
+++ practice-2017-09-30-android/MyApplication/domain/src/test/java/com/example/MyClassTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dev on 30/09/2017.
+ */
+public class MyClassTest {
+
+    @Test
+    public void test_that_tests_are_working() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
