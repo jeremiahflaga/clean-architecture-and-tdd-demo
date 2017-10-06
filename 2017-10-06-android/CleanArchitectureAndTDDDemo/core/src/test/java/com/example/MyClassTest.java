7ca10054ab04d5c27c22aedc35b28995dd05b2bf
Jboy Flaga <flaga.jeremiah@gmail.com>
2017-10-06 11:03:06 +0800

    test that the tests environment is working fine.

---
+++ 2017-10-06-android/CleanArchitectureAndTDDDemo/core/src/test/java/com/example/MyClassTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MyndDev on 06/10/2017.
+ */
+public class MyClassTest {
+
+}
---
+++ 2017-10-06-android/CleanArchitectureAndTDDDemo/core/src/test/java/com/example/interactors/createcat/CreateCatInteractorTests.java
@@ -0,0 +1,15 @@
+package com.example.interactors.createcat;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by MyndDev on 06/10/2017.
+ */
+public class CreateCatInteractorTests {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
