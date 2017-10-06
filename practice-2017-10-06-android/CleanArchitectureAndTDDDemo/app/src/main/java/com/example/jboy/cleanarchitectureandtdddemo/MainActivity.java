996dd181b83b60b1d25c74775e1a6a85bf22bf43
Jboy Flaga <flaga.jeremiah@gmail.com>
2017-10-06 14:58:03 +0800

    UI

--- practice-2017-10-06-android/CleanArchitectureAndTDDDemo/app/src/main/java/com/example/jboy/cleanarchitectureandtdddemo/MainActivity.java
+++ practice-2017-10-06-android/CleanArchitectureAndTDDDemo/app/src/main/java/com/example/jboy/cleanarchitectureandtdddemo/MainActivity.java
@@ -2,28 +2,51 @@ package com.example.jboy.cleanarchitectureandtdddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.data.CatsRepositoryImpl;
 import com.example.interactors.createcat.CatCreatedPresenter;
 import com.example.interactors.createcat.CreateCatInteractor;
+import com.example.interactors.createcat.CreateCatRequest;
 import com.example.presentation.createcat.CatCreatedPresenterImpl;
 import com.example.presentation.createcat.CatCreatedView;
 import com.example.presentation.createcat.CatCreatedViewModel;
+import com.example.repositories.CatsRepository;
 
 public class MainActivity extends AppCompatActivity implements CatCreatedView {
 
     TextView nameTextView;
+    EditText nameEditText;
+    EditText titleEditText;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameTextView = (TextView) findViewById(R.id.name_textview);
+        nameTextView = (TextView) findViewById(R.id.grandios_name_textview);
+        nameEditText = (EditText) findViewById(R.id.name_edittext);
+        titleEditText = (EditText) findViewById(R.id.title_edittext);
+        saveButton = (Button) findViewById(R.id.save_button);
 
         CatCreatedPresenter presenter = new CatCreatedPresenterImpl(this);
-        CreateCatInteractor interactor = new CreateCatInteractor(presenter);
-        interactor.execute(request);
+        CatsRepositoryImpl repository = new CatsRepositoryImpl();
+        final CreateCatInteractor interactor = new CreateCatInteractor(presenter, repository);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateCatRequest request = new CreateCatRequest();
+                request.name = nameEditText.getText().toString();
+                request.title = titleEditText.getText().toString();
+
+                interactor.execute(request);
+            }
+        });
     }
 
     @Override
