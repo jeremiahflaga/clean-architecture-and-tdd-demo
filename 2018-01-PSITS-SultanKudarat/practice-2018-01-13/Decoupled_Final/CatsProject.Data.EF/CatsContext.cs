a4efac765aff43e9690f154153f3e6f829d1a4be
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2018-02-03 23:34:54 +0800

    added images of Hav I been Pwned; also, DbContext of EF

--- 2018-01-PSITS-SultanKudarat/practice-2018-01-13/Decoupled_Final/CatsProject.Data.EF/CatsContext.cs
+++ 2018-01-PSITS-SultanKudarat/practice-2018-01-13/Decoupled_Final/CatsProject.Data.EF/CatsContext.cs
@@ -1,12 +1,14 @@
-﻿//using JetBrains.Annotations;
-//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace CatsProject.Data.EF
-//{
-//    internal class CatsContext : DbContext
-//    {
-//    }
-//}
+namespace CatsProject.Data.EF
+{
+    internal class CatsContext : DbContext
+    {
+        {
+        }
+    }
+}
